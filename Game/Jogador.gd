311ee112ada18db30d82420dbc29e180aa4f46fd
lhfaguiar <lais.harumi.f@gmail.com>
2020-12-03 15:03:18 -0300

    cria func proccess do jogador

--- Game/Jogador.gd
+++ Game/Jogador.gd
@@ -13,7 +13,20 @@ func _ready():
 	screen_size = get_viewport_rect().size
 
 func _proccess():
-	pass
+	var velocity = Vector2()  # The player's movement vector.
+	if Input.is_action_pressed("ui_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("ui_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("ui_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("ui_up"):
+		velocity.y -= 1
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+		$AnimatedSprite.play()
+	else:
+		$AnimatedSprite.stop()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
